8ec4b256afbf7982c685380abfb36a4a8e48e089
ShubhamPrakash <d1901.bca@gmail.com>
2022-07-25 18:24:12 +0530

    filters done

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 
 const inputFile =document.querySelector(".file-input"),
  filterOptions = document.querySelectorAll(".filter button"),
- chooseImgBtn = document.querySelector(".choose-img"),
- previewImg = document.querySelector(".preview-img img"),
- container = document.querySelector(".container"),
- filterSlider = document.querySelector(".slider input"),
+ filterName = document.querySelector(".filter-info .name"),
  sliderValue=document.querySelector(".slider .value"),
- filterName = document.querySelector(".filter-info .name")
+ filterSlider = document.querySelector(".slider input"),
+ container = document.querySelector(".container"),
+
+ previewImg = document.querySelector(".preview-img img"),
+ chooseImgBtn = document.querySelector(".choose-img")
 ;
+
+let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
+
+function applyFilters(){
+    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+    // console.log(brightness);
+}
 
 
 const loadImg = ()=>{
@@ -28,13 +36,49 @@ filterOptions.forEach(option =>{
         document.querySelector(".filter .active").classList.remove("active");
         option.classList.add("active");
         filterName.textContent = option.textContent;
+
+        if(option.id === "brightness"){
+            filterSlider.max = "200";
+            filterSlider.value = brightness;
+            sliderValue.textContent = `${brightness}`;
+        }else if(option.id === "saturation"){
+            filterSlider.max = "200";
+            filterSlider.value = saturation;
+            sliderValue.textContent = `${saturation}`;
+        }else if(option.id === "inversion"){
+           filterSlider.max = "100";
+            filterSlider.value = inversion;
+            sliderValue.textContent = `${inversion}`;
+        }else{
+            filterSlider.max = "100";
+            filterSlider.value = grayscale;
+            sliderValue.textContent = `${grayscale}`;
+        }
     });
 });
 
-const updateFilter = ()=> {
-    console.log(filterSlider.value);
-    sliderValue.innerText = `${parseInt(filterSlider.value) } %`;
-}
+
+/*  *********** Changing and printing the slider value  */
+
+function updateFilter(){
+    // console.log(filterSlider.value);
+    sliderValue.innerText = `${filterSlider.value } %`;
+    const selectedFilter = document.querySelector(".filter .active")
+
+    if(selectedFilter.id === "brightness" ){
+        brightness = filterSlider.value;
+    }
+    else if(selectedFilter.id === "saturation"){
+        saturation = filterSlider.value;
+    }
+    else if(selectedFilter.id === "inversion"){
+        inversion = filterSlider.value;
+    }
+    else {
+        grayscale = filterSlider.value;
+    }
+    applyFilters();
+};
 
 inputFile.addEventListener("change", loadImg);
 filterSlider.addEventListener("input", updateFilter);
